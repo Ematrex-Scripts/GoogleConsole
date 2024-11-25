2ab3e2075d96a36a5736afd82d536e14901441d4
Your Name <you@example.com>
2024-11-25 15:01:32 +0100

    push

--- commands.sh
+++ commands.sh
@@ -1,10 +1,5 @@
-
 sudo apt install git  -y
-
 git clone https://github.com/Ematrex-Scripts/GoogleConsole.git
-
 cd GoogleConsole && chmod 777 * 
-
 bash postfix.sh 
-
 nohup python3 main.py $1   $2 > output.log 2>&1 &
